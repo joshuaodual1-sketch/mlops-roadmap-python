4d794581ea29ea671900c8926ba04d1e1ec9c68d
joshuaodual1-sketch <joshuaodual1.com>
2026-05-04 01:38:35 +0100

    feat: I tried adding spaces inside and outside the quotes to chcek which of them works and how it could go wrong to createa feedback and method one, and its always logical to add your space within the quotes whenever using Method 1.

--- 01_Python_Fundamentals/hello.py
+++ 01_Python_Fundamentals/hello.py
@@ -1,11 +1,6 @@
-# 4 Ways to buide a string
-# MEthod 1 concatenation with +
-name = input("What's your name?")
+#ouside quotes
+name = input("What's your name? ")
+print("Hello,"  + name)
+#inside quotes
+name = input("What's your name? ")
 print("Hello, " + name)
-# Method 2 Multiple arguments (COMMA)
-print("Hello,", name) # auto inserts a space
-#Method 3 Named Parameters (End/Sep)
-print("Hello, ", end="")
-print(name)
-#Method 4 F- strings (higly recommended)
-print(f"Hello, {name}")
