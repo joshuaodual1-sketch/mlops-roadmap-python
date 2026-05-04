070ef7ce5e70a9a4ca4ebea14b0816b329122793
joshuaodual1-sketch <joshuaodual1.com>
2026-05-04 03:57:53 +0100

    feat: made a basic calculator

--- 01_Python_Fundamentals/hello.py
+++ 01_Python_Fundamentals/hello.py
@@ -1,21 +1,16 @@
-# String manipulation and user input. 
-# This program asks the user for their name, removes any leading or trailing whitespace, converts it to title case, and then greets the user.
-name = input("What is your name? ")
-name = name.strip().title()
-print(f"Hello, {name}!")
-
-# String manipulation and user input. 
-#slipting the full name into first and last name.
-full_name = input("What is your name? ")
-first_name, last_name = full_name.split()
-print(f"Hello, {first_name}.")
-#String manipulation and user input.
-#This program asks the user for their city, converts it to title case, and then comments on the city.   
-city = input("What city do you live in? ")
-city = city.title()
-print(f"{city} is a great place to live!")
-# String manipulation and user input.
-#This program asks the user for their name, converts it to uppercase and then lowercase, and
-name = input("What is your name? ")
-name = name.upper().lower()
-print(f"Hello, {name}!")
+#calculator challenge:sum
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+print(x + y)
+#calculator challenge:product
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+print(x * y)
+#calculator challenge: quotient
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+print(x / y)
+#calculator challenge: difference
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+print(x - y)
