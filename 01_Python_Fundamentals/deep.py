3aa80f0813fa8d39f9705812e719bc4751dd06f7
joshuaodual1-sketch <joshuaodual1.com>
2026-05-07 19:53:17 +0100

    feat: add deep.py to implement input matching for the answer to life

---
+++ 01_Python_Fundamentals/deep.py
@@ -0,0 +1,19 @@
+deep = (input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")).strip().lower()
+
+
+match deep:
+    case "42" | "forty two" | "forty-two":
+        print("Yes")
+    case _:
+        print("No")
+
+
+    
+
+
+
+
+
+
+
+
